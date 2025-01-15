18dcde0a3b4d2721692fb553580b8ce4d0c1f31f
Donald Winkelman <dmwink.tx@gmail.com>
2025-01-14 22:27:30 -0600

    Started work on tracks accordion

---
+++ my-app/src/lib/components/tracks.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import * as Card from "$lib/components/ui/card";
+    import * as Accordion from "$lib/components/ui/accordion/index.js";
+</script>
+
+<Accordion.Root class="m-14">
+  <Accordion.Item value="item-1">
+    <Accordion.Trigger class="text-2xl text-emerald-500 w-full">
+      <div class="w-full flex items-center justify-center">
+        <span class="flex-1 text-center">Project Track</span>
+      </div>
+    </Accordion.Trigger>
+    <Accordion.Content>
+      The project track is where you make projects :>
+    </Accordion.Content>
+  </Accordion.Item>
+  <Accordion.Item value="item-2">
+    <Accordion.Trigger class="text-2xl text-indigo-500 w-full">
+      <div class="w-full flex items-center justify-center">
+        <span class="flex-1 text-center">Research Track</span>
+      </div>
+    </Accordion.Trigger>
+    <Accordion.Content>
+      The research track is where you read cool research papers and maybe other things?? :&
+    </Accordion.Content>
+  </Accordion.Item>
+  <Accordion.Item value="item-3">
+    <Accordion.Trigger class="text-2xl text-rose-500 w-full">
+      <div class="w-full flex items-center justify-center">
+        <span class="flex-1 text-center">Competition Track</span>
+      </div>
+    </Accordion.Trigger>
+    <Accordion.Content>
+      The competition track is where you compete in short ML model coding competitions &lt):o
+    </Accordion.Content>
+  </Accordion.Item>
+</Accordion.Root>
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
   import Calendar from "$lib/components/calendar.svelte"
   import PrevCalendar from "$lib/components/prev_calendar.svelte"
+  import Tracks from "$lib/components/tracks.svelte"
 </script>
 <Calendar />
 <PrevCalendar />
+<Tracks />
