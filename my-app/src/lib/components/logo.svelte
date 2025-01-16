4186302427763f78e584deec43f9ef2625295092
Shriya Gautam <shriya.gautam5@gmail.com>
2025-01-15 16:27:41 -0800

    changed sizing of logo bar

--- my-app/src/lib/components/logo.svelte
+++ my-app/src/lib/components/logo.svelte
@@ -1,11 +1,12 @@
-<div class="flex justify-center items-center gap-2">
+<div class="flex justify-center items-center gap-2 pb-6 pt-6">
   <span>
-    <img src="images/ml-logo-border.png" height="465" width="681" alt="ml-logo" />
+    <img src="images/ml-logo-border.png" height="125" width="125" alt="ml-logo" />
   </span>
-  <span class="fira justify-center text-9xl bg-gradient-to-l from-umass-black to-umass-maroon bg-clip-text text-transparent font-bold inline-block">
+  <span class="fira justify-center text-6xl bg-gradient-to-l from-umass-black to-umass-maroon bg-clip-text text-transparent font-bold inline-block">
     {@html '{MassAI}'}
   </span>
 </div>
+
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;500;700&display=swap');
   .fira {
