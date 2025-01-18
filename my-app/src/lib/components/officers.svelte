1feda32b9647c48c263bb1f964e77b91388bd7fa
Donald Winkelman <dmwink.tx@gmail.com>
2025-01-18 15:31:04 -0600

    Added officers to the about us

--- my-app/src/lib/components/officers.svelte
+++ my-app/src/lib/components/officers.svelte
@@ -2,28 +2,34 @@
     const eBoardMembers = [
         {
             name: " Donald Winkelman",
-            title: "Co-President",
+            title: "President of Outreach",
             picture: "/images/donnie.png",
             bio: "Hi y’all, I’m Donnie! I’m in the MS CS program at UMass and delving further into deep learning! I’ve done some research in interpretability and safety (exploring how models think) alongside experiments with computer vision models. I enjoy reading papers in medicine and ML and want to specialize in medical AI to increase diagnostic accuracy, surgical accuracy, enable the development of more complex laboratory-made proteins, and much more. Look forward to meeting y'all!"
         },
         {
             name: " Aryaman Tepal",
-            title: "Co-President",
+            title: "President of Education",
             picture: "/images/aryaman.png",
             bio: "Coming soon..."
         },
         {
             name: "Niko Lorantos",
-            title: "Co-Vice President",
-            picture: "/images/ml-logo-border.png",
-            bio: "Coming soon... "
+            title: "Vice President of Education",
+            picture: "/images/niko.jpg",
+            bio: "Niko Lorantos is an honors student studying computational mathematics and minoring in philosophy. He is interested in all things AI, from industry trends to niche research topics. He is currently involved in artificial life research at Amherst College."
         },
         {
             name: "Shriya Gautam",
-            title: "Co-Vice President",
+            title: "Vice President of Outreach",
             picture: "/images/ml-logo-border.png",
             bio: "Coming soon..."
         },
+        {
+            name: "Adriana Caraeni",
+            title: "Events Coordinator / Lecturer",
+            picture: "/images/adriana.jpg",
+            bio: "Honors Dual Degree in CS + Math plus Business Minor. I like rollerblading, biking, dancing, skiiing,  ..."
+        }
     ];
 </script>
 
