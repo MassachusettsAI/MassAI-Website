e7780127c9d34f8b475e1edd03bfb44afa6bceaa
Donald Winkelman <dmwink.tx@gmail.com>
2025-01-19 15:39:36 -0600

    Added research track accordion

--- my-app/src/lib/components/tracks/research_track.svelte
+++ my-app/src/lib/components/tracks/research_track.svelte
@@ -1,1 +1,65 @@
-The research track is where you read cool research papers and maybe other things?? :&
+<script lang="ts">
+    import { buttonVariants } from "$lib/components/ui/button";
+</script>
+<div class="text-lg text-center">
+  Research is at the core of Machine Learning, and our paper reading group will give you the foundational knowledge necessary to run your own experiments.
+  Our paper reading group meets every week and is open to newcomers at any time!
+</div>
+<ol class="relative m-4 border-s border-gray-200 dark:border-gray-700">
+  <li class="mb-4 ms-6">
+    <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+      <svg class="w-5 h-5 text-blue-400 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+        <path fill-rule="evenodd" d="M11 4.717c-2.286-.58-4.16-.756-7.045-.71A1.99 1.99 0 0 0 2 6v11c0 1.133.934 2.022 2.044 2.007 2.759-.038 4.5.16 6.956.791V4.717Zm2 15.081c2.456-.631 4.198-.829 6.956-.791A2.013 2.013 0 0 0 22 16.999V6a1.99 1.99 0 0 0-1.955-1.993c-2.885-.046-4.76.13-7.045.71v15.081Z" clip-rule="evenodd"/>
+      </svg>
+    </span>
+    <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+      Read Paper of the Week
+    </h3>
+    <time class="block mb-2 text-base text-md font-normal leading-none text-gray-400 dark:text-gray-500">
+      Before Meeting
+    </time>
+  </li>
+  <li class="mb-4 ms-6">
+    <span class="absolute flex items-center justify-center w-6 h-6 bg-sky-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+      <svg class="w-5 h-5 text-sky-400 dark:text-sky-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+        <path fill-rule="evenodd" d="M8 7V2.221a2 2 0 0 0-.5.365L3.586 6.5a2 2 0 0 0-.365.5H8Zm2 0V2h7a2 2 0 0 1 2 2v.126a5.087 5.087 0 0 0-4.74 1.368v.001l-6.642 6.642a3 3 0 0 0-.82 1.532l-.74 3.692a3 3 0 0 0 3.53 3.53l3.694-.738a3 3 0 0 0 1.532-.82L19 15.149V20a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V9h5a2 2 0 0 0 2-2Z" clip-rule="evenodd"/>
+        <path fill-rule="evenodd" d="M17.447 8.08a1.087 1.087 0 0 1 1.187.238l.002.001a1.088 1.088 0 0 1 0 1.539l-.377.377-1.54-1.542.373-.374.002-.001c.1-.102.22-.182.353-.237Zm-2.143 2.027-4.644 4.644-.385 1.924 1.925-.385 4.644-4.642-1.54-1.54Zm2.56-4.11a3.087 3.087 0 0 0-2.187.909l-6.645 6.645a1 1 0 0 0-.274.51l-.739 3.693a1 1 0 0 0 1.177 1.176l3.693-.738a1 1 0 0 0 .51-.274l6.65-6.646a3.088 3.088 0 0 0-2.185-5.275Z" clip-rule="evenodd"/>
+      </svg>
+    </span>
+    <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+      Review Paper
+    </h3>
+    <time class="block mb-2 text-base text-md font-normal leading-none text-gray-400 dark:text-gray-500">
+      10 Minutes
+    </time>
+  </li>
+  <li class="mb-4 ms-6">
+    <span class="absolute flex items-center justify-center w-6 h-6 bg-cyan-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+      <svg class="w-5 h-5 text-cyan-400 dark:text-cyan-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+        <path fill-rule="evenodd" d="M3.559 4.544c.355-.35.834-.544 1.33-.544H19.11c.496 0 .975.194 1.33.544.356.35.559.829.559 1.331v9.25c0 .502-.203.981-.559 1.331-.355.35-.834.544-1.33.544H15.5l-2.7 3.6a1 1 0 0 1-1.6 0L8.5 17H4.889c-.496 0-.975-.194-1.33-.544A1.868 1.868 0 0 1 3 15.125v-9.25c0-.502.203-.981.559-1.331ZM7.556 7.5a1 1 0 1 0 0 2h8a1 1 0 0 0 0-2h-8Zm0 3.5a1 1 0 1 0 0 2H12a1 1 0 1 0 0-2H7.556Z" clip-rule="evenodd"/>
+      </svg>
+    </span>
+    <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+      Open-Floor Discussion
+    </h3>
+    <time class="block mb-2 text-base text-md font-normal leading-none text-gray-400 dark:text-gray-500">
+      20-40 Minutes
+    </time>
+  </li>
+  <li class="mb-4 ms-6">
+    <span class="absolute flex items-center justify-center w-6 h-6 bg-teal-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+      <svg class="w-5 h-5 text-teal-400 dark:text-teal-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+        <path fill-rule="evenodd" d="M18 5.05h1a2 2 0 0 1 2 2v2H3v-2a2 2 0 0 1 2-2h1v-1a1 1 0 1 1 2 0v1h3v-1a1 1 0 1 1 2 0v1h3v-1a1 1 0 1 1 2 0v1Zm-15 6v8a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-8H3ZM11 18a1 1 0 1 0 2 0v-1h1a1 1 0 1 0 0-2h-1v-1a1 1 0 1 0-2 0v1h-1a1 1 0 1 0 0 2h1v1Z" clip-rule="evenodd"/>
+      </svg>
+    </span>
+    <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+      Propose and Vote On Next Week's Paper
+    </h3>
+    <time class="block mb-2 text-base text-md font-normal leading-none text-gray-400 dark:text-gray-500">
+      10 Minutes
+    </time>
+  </li>
+</ol>
+<a href="https://docs.google.com/spreadsheets/d/1t-JOOxt4bM4WuBwo-UWLwPoVCHLf3QvYGoXmCTTtsdk/edit?usp=sharing" class={buttonVariants({ variant: "outline" })}>
+  List of Milestone AI Papers
+</a>
