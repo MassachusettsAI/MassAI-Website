dfd2a0937656ecb7d57a496103147bcce23800c5
Donald Winkelman <dmwink.tx@gmail.com>
2025-01-19 16:21:08 -0600

    Added competition track text

--- my-app/src/lib/components/tracks/competition_track.svelte
+++ my-app/src/lib/components/tracks/competition_track.svelte
@@ -1,1 +1,3 @@
-The competition track is where you compete in short ML model coding competitions &lt):o
+<div class="text-lg text-center mb-3">
+  Put your coding skills to the test in lightning-fast model coding competitions! We anticipate hosting 2 competitions this semester.
+</div>
