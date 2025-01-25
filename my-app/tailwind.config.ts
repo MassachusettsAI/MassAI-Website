6a6d71177c464db4e3298008a363157448b1a0d8
Donald Winkelman <dmwink.tx@gmail.com>
2025-01-24 20:44:11 -0600

    Fixing inter not working

--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -113,19 +113,9 @@ const config: Config = {
 					'serif'
 				],
 				sans: [
-					'system-ui',
-					'-apple-system',
-					'BlinkMacSystemFont',
-					'"Segoe UI"',
-					'Roboto',
-					'"Helvetica Neue"',
-					'Arial',
-					'"Noto Sans"',
-					'sans-serif',
-					'"Apple Color Emoji"',
-					'"Segoe UI Emoji"',
-					'"Segoe UI Symbol"',
-					'"Noto Color Emoji"',
+					'Inter var',
+					'Inter',
+					...fontFamily.sans  // This spreads the default system fonts
 				],
 			},
 		}
