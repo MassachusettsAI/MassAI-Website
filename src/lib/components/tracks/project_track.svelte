75ee30227522850e886dd20a6eb3d195759b93e8
Donald Winkelman <dmwink.tx@gmail.com>
2025-01-31 01:08:55 -0500

    Making changes to projects accordion

--- src/lib/components/tracks/project_track.svelte
+++ src/lib/components/tracks/project_track.svelte
@@ -16,13 +16,26 @@
     </time>
   </li>
   <li class="mb-4 ms-6">
+    <span class="absolute flex items-center justify-center w-6 h-6 bg-green-100 rounded-full -start-3 ring-8 ring-white dark:ring-slate-800 dark:bg-green-200">
+      <svg class="w-6 h-6 text-green-400 dark:text-green-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+        <path fill-rule="evenodd" d="M12 6a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7Zm-1.5 8a4 4 0 0 0-4 4 2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-3Zm6.82-3.096a5.51 5.51 0 0 0-2.797-6.293 3.5 3.5 0 1 1 2.796 6.292ZM19.5 18h.5a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-1.1a5.503 5.503 0 0 1-.471.762A5.998 5.998 0 0 1 19.5 18ZM4 7.5a3.5 3.5 0 0 1 5.477-2.889 5.5 5.5 0 0 0-2.796 6.293A3.501 3.501 0 0 1 4 7.5ZM7.1 12H6a4 4 0 0 0-4 4 2 2 0 0 0 2 2h.5a5.998 5.998 0 0 1 3.071-5.238A5.505 5.505 0 0 1 7.1 12Z" clip-rule="evenodd"/>
+      </svg>
+    </span>
+    <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+      Team Finalization Meeting
+    </h3>
+    <time class="block mb-2 text-base text-md font-normal leading-none text-gray-400">
+      Thursday, February 13, 2025
+    </time>
+  </li>
+  <li class="mb-4 ms-6">
     <span class="absolute flex items-center justify-center w-6 h-6 bg-lime-100 rounded-full -start-3 ring-8 ring-white dark:ring-slate-800 dark:bg-lime-200">
       <svg class="w-6 h-6 text-lime-400 dark:text-lime-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4.5V19a1 1 0 0 0 1 1h15M7 10l4 4 4-4 5 5m0 0h-3.207M20 15v-3.207"/>
       </svg>
     </span>
     <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-      Proposal + First Model Chart Due
+      Proposal Due
     </h3>
     <time class="block mb-2 text-base text-md font-normal leading-none text-gray-400">
       Thursday, February 20, 2025
@@ -35,7 +48,7 @@
       </svg>
     </span>
     <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-      Experiment Progress Check-In
+      Experiment Progress Check-In + First Model Charts Due
     </h3>
     <time class="block mb-2 text-base text-md font-normal leading-none text-gray-400">
       Thursday, March 6, 2025
