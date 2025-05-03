b3fb5ffc60fa825699f486b72e99c7bf63d8a122
Shriya Gautam <57238679+bits-and-bytes85@users.noreply.github.com>
2025-05-03 15:02:19 -0400

    Update lectures.svelte
    added fall lectures + slides

--- src/lib/components/lectures.svelte
+++ src/lib/components/lectures.svelte
@@ -3,6 +3,38 @@
 
     export let lectures = [
         {
+            title: "Linear Regression",
+            date: "Sept 15, 2024",
+            videoUrl: "https://www.youtube.com/watch?v=CtsRRUddV2s",
+            resources: [
+                { name: "Lecture Slides", url: "https://docs.google.com/presentation/d/11aCbIRESe8AFEdSRV7NR3MSYzFPR7VOrAtpt1UBbD3k/edit?usp=sharing" },
+            ]
+        },
+        {
+            title: "Natural Language Processing & Transformers",
+            date: "Oct 10, 2024, 2024",
+            videoUrl: "https://www.youtube.com/@MassAI-i4z",
+            resources: [
+                { name: "Lecture Slides", url: "https://docs.google.com/presentation/d/1_Ka4xxVkcposHTfyMAPelDVm0Gim47xtAdxbN1hzjBc/edit?usp=sharing" },
+            ]
+        },
+        {
+            title: "Neural Networks",
+            date: "October 20, 2024",
+            videoUrl: "https://www.youtube.com/watch?v=jmmW0F0biz0",
+            resources: [
+                { name: "Lecture Slides", url: "https://docs.google.com/presentation/d/1kpWqDDNQ3mwsyamMgjVBqBbyXhBujRQoWL4Kbe12QrU/edit?usp=sharing" },
+            ]
+        },
+        {
+            title: "Computer Vision",
+            date: "Nov 3, 2024, 2024",
+            videoUrl: "https://www.youtube.com/@MassAI-i4z",
+            resources: [
+                { name: "Lecture Slides", url: "https://docs.google.com/presentation/d/1Z4zyjYRMc2mmLlQyTKxwEj4R9sEPgtUKIOQWUmHt6eQ/edit?usp=sharing" },
+            ]
+        },
+        {
             title: "Introduction to Reinforcement Learning",
             date: "November 10, 2024",
             videoUrl: "https://www.youtube.com/watch?v=1-TvSgOuAH4&t=4s",
@@ -26,32 +58,7 @@
             resources: [
                 { name: "Lecture Slides", url: "https://docs.google.com/presentation/d/1ZNcRx5P04liyKTB39_RAylJONGmkrs-Fof2Xs8hOjac/edit?usp=sharing" },
             ]
-        },
-        {
-            title: "Neural Networks",
-            date: "October 20, 2024",
-            videoUrl: "https://www.youtube.com/watch?v=jmmW0F0biz0",
-            resources: [
-                { name: "Lecture Slides", url: "https://docs.google.com/presentation/d/1kpWqDDNQ3mwsyamMgjVBqBbyXhBujRQoWL4Kbe12QrU/edit?usp=sharing" },
-            ]
-        },
-        {
-            title: "Linear Regression",
-            date: "Sept 15, 2024",
-            videoUrl: "",
-            resources: [
-                { name: "Lecture Slides", url: "https://docs.google.com/presentation/d/11aCbIRESe8AFEdSRV7NR3MSYzFPR7VOrAtpt1UBbD3k/edit?usp=sharing" },
-            ]
-        },
-        {
-            title: "Natural Language Processing & Transformers",
-            date: "Nov 20, 2024, 2024",
-            videoUrl: "",
-            resources: [
-                { name: "Lecture Slides", url: "https://docs.google.com/presentation/d/1_Ka4xxVkcposHTfyMAPelDVm0Gim47xtAdxbN1hzjBc/edit?usp=sharing" },
-            ]
         }
-        
     ];
 
     function getEmbedUrl(url) {
