46038fd0785ff1ac3168e7249ca94a9c10a426be
Niko Lorantos <lorantos32@gmail.com>
2025-07-01 07:30:12 -0400

    footer logo

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,6 +1,6 @@
 <!-- Footer.svelte -->
 <script>
-  export let logoSrc = "/static/images/ml-logo-border.png"; 
+  export let logoSrc = "/static/images/favicon-32x32.png"; 
   export let logoAlt = "MassAI Logo";
   export let copyrightText = `© ${new Date().getFullYear()} MassAI. All rights reserved.`;
   export let showSocialLinks = true;
