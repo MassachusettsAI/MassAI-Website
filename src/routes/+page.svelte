9fcc2e1f0d082cd31679577158e5f1d1a457bb89
Shriya Gautam <57238679+bits-and-bytes85@users.noreply.github.com>
2026-01-12 15:51:10 -0800

    added officer application banner
    Updated the banner visibility and changed the link text.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,15 +8,15 @@
   import Backdrop from "$lib/components/backdrop.svelte";
   import Lectures from "$lib/components/lectures.svelte";
   import Footer from "$lib/components/footer.svelte";
-  let showBanner = false;
+  let showBanner = true;
 </script>
 
 <Navbar />
 
 <div class="relative">
   {#if showBanner}
-    <a href="https://forms.gle/Fv9UyQnhCicxHZka8" target="_blank" class="block text-center h-20 bg-emerald-600 text-lg text-white hover:bg-emerald-700 transition-colors flex items-center justify-center font-medium p-4 shadow-md mt-16 pb-1">
-      Project Matching is Open! Click Here to Build a Project with MassAI!
+    <a href="https://docs.google.com/forms/d/e/1FAIpQLSd9BIhuO5XlXsrIIMk1OiV4KDpnkzgZ007b1wnIknLbso5X8g/viewform" target="_blank" class="block text-center h-20 bg-emerald-600 text-lg text-white hover:bg-emerald-700 transition-colors flex items-center justify-center font-medium p-4 shadow-md mt-16 pb-1">
+      Apply to join the MassAI Eboard for the Spring 2026 semester!
       <button on:click|preventDefault={() => showBanner = false} class="absolute right-4 top-1/2 -translate-y-1/2 text-2xl">&times;</button>
     </a>
   {:else}
